b8f7182be2a2d61f02739313cc84fbcb5d0eb760
naren-black <naren.nath7@gmail.com>
2021-11-21 12:28:02 +1100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import { Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherpage" component={OtherPage} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
