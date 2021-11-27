0c5c3c822ef6ae64f418d55267fa990e8ffd1818
naren-black <naren.nath7@gmail.com>
2021-11-28 09:21:02 +1100

    Added react logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import { Router, Route, Link } from 'react-router-dom';
@@ -10,8 +10,23 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Fib} />
-        <Route path="/otherpage" component={OtherPage} />
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Fibonacci Calculator version 2
+            </a>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
     </Router>
   );
